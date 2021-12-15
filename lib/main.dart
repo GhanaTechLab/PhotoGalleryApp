9127d4321380325cde1e2172170c99619a559649
Etornam <sunumacbright@gmail.com>
2021-12-15 16:03:31 +0000

    initial code push

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:photo_gallery_mobile_app/views/home_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PhotoGallery',
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: const AppBarTheme(
+              elevation: 1,
+              backgroundColor: Colors.white,
+              iconTheme: IconThemeData(color: Colors.blue))),
+      darkTheme: ThemeData.dark().copyWith(
+          appBarTheme: const AppBarTheme(
+              elevation: 1, iconTheme: IconThemeData(color: Colors.blue))),
+      themeMode: ThemeMode.system,
+      home: const HomeView(),
+    );
+  }
+}
